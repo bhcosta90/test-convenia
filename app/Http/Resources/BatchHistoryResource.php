538c7fdf06bad37c93d3d3596323bca89ae32c31
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 11:03:10 -0300

    add BatchHistoryResource for transforming batch history data into JSON format

---
+++ app/Http/Resources/BatchHistoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\BatchHistory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin BatchHistory
+ */
+final class BatchHistoryResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'batch_id' => $this->batch_id,
+            'data' => $this->data,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
