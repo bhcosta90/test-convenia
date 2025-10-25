b218a45725b7bc7f81c0b949b7203fc867b2dbbf
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 08:26:35 -0300

    add employees relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,17 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 final class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use Notifiable;
 
@@ -36,6 +36,11 @@ final class User extends Authenticatable
         'remember_token',
     ];
 
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -45,7 +50,7 @@ final class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password'          => 'hashed',
+            'password' => 'hashed',
         ];
     }
 }
