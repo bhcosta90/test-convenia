3ad4d02cf36e939d115331baa8a6a68342e7fe4d
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 11:13:09 -0300

    add UploadFileSuccessNotification class for successful file upload notifications

---
+++ app/Notifications/User/UploadFileSuccessNotification.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications\User;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+final class UploadFileSuccessNotification extends Notification
+{
+    public function __construct() {}
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+}
