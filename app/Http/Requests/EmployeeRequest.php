feff98fe7a9cfd9ba36fd4c63def9f483750526a
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 09:23:06 -0300

    refactor EmployeeRequest to enhance CPF validation and ensure unique email per user

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Models\Employee;
+use App\Rules\CpfRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 final class EmployeeRequest extends FormRequest
 {
@@ -12,8 +15,8 @@ final class EmployeeRequest extends FormRequest
     {
         return [
             'name' => ['required'],
-            'email' => ['required', 'email', 'max:254'],
-            'cpf' => ['required'],
+            'email' => ['required', 'email', 'max:254', Rule::unique(Employee::class)->where('user_id', $this->user()->id)->ignore($this->employee?->id)],
+            'cpf' => ['required', new CpfRule(), Rule::unique(Employee::class)->where('user_id', $this->user()->id)->ignore($this->employee?->id)],
             'city' => ['required'],
             'state' => ['required'],
         ];
