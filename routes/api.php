a1d306d61b5ffb7628980175265cff688161829d
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 09:15:41 -0300

    refactor API routes to improve authentication middleware structure

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('employees', Controllers\EmployeeController::class);
-Route::prefix('auth')->controller(Controllers\AuthController::class)
-    ->middleware(['auth:api'])
-    ->group(function () {
-        Route::post('login', 'login')->withoutMiddleware('auth:api');
-        Route::post('refresh', 'refresh')->withoutMiddleware('auth:api');
-        Route::delete('logout', 'logout');
-    });
+Route::middleware('auth')->group(function () {
+    Route::apiResource('employees', Controllers\EmployeeController::class);
+    Route::controller(Controllers\AuthController::class)
+        ->middleware(['auth:api'])
+        ->group(function () {
+            Route::post('login', 'login')->withoutMiddleware('auth:api');
+            Route::post('refresh', 'refresh')->withoutMiddleware('auth:api');
+            Route::delete('logout', 'logout');
+        });
+});
