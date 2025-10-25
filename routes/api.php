82737a84a1de96d9644076886c4bccd8af17b812
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 09:04:28 -0300

    add authentication routes for login, refresh, and logout in api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('employees', Controllers\EmployeeController::class);
+Route::prefix('auth')->controller(Controllers\AuthController::class)
+    ->middleware(['auth:api'])
+    ->group(function () {
+        Route::post('login', 'login')->withoutMiddleware('auth:api');
+        Route::post('refresh', 'refresh')->withoutMiddleware('auth:api');
+        Route::post('logout', 'logout');
+    });
