e0514f5647c04e1d2e83763d22628a1350640e2a
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 08:22:05 -0300

    create Employee model with fillable attributes

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+final class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cpf',
+        'city',
+        'state',
+    ];
+}
