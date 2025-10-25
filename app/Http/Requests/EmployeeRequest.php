cc95bccaa02d04dd4ab0e456dd24f79d0cae6b3d
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 10:05:30 -0300

    refactor EmployeeRequest to use EmployeeValidation for rules management

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -4,50 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use App\Models\Employee;
-use App\Rules\CpfRule;
+use App\Http\Validation\EmployeeValidation;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\RequiredIf;
 
 final class EmployeeRequest extends FormRequest
 {
     public function rules(): array
     {
-        $id = $this->route('employee');
+        $validation = app(EmployeeValidation::class);
 
-        return [
-            'name' => [
-                new RequiredIf(! $id),
-                'string',
-                'min:1',
-            ],
-            'email' => [
-                new RequiredIf(! $id),
-                'string',
-                'min:1',
-                'email:rfc,filter',
-                'max:254',
-                Rule::unique(Employee::class)->where('user_id', $this->user()->id)->ignore($this->employee?->id),
-            ],
-            'cpf' => [
-                new RequiredIf(! $id),
-                'string',
-                'min:1',
-                new CpfRule(),
-                Rule::unique(Employee::class)->where('user_id', $this->user()->id)->ignore($this->employee?->id),
-            ],
-            'city' => [
-                new RequiredIf(! $id),
-                'string',
-                'min:1',
-            ],
-            'state' => [
-                new RequiredIf(! $id),
-                'string',
-                'min:1',
-            ],
-        ];
+        return $validation->make($this->route('employee'));
     }
 
     public function authorize(): bool
---
+++ app/Http/Validation/EmployeeValidation.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Validation;
+
+use App\Models\Employee;
+use App\Models\User;
+use App\Rules\CpfRule;
+use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\RequiredIf;
+
+final readonly class EmployeeValidation
+{
+    public function __construct(#[CurrentUser] private User $user)
+    {
+        //
+    }
+
+    public function make(?int $id): array
+    {
+        return [
+            'name' => [
+                new RequiredIf(! $id),
+                'string',
+                'min:1',
+            ],
+            'email' => [
+                new RequiredIf(! $id),
+                'string',
+                'min:1',
+                'email:rfc,filter',
+                'max:254',
+                Rule::unique(Employee::class)->where('user_id', $this->user->id)->ignore($id),
+            ],
+            'cpf' => [
+                new RequiredIf(! $id),
+                'string',
+                'min:1',
+                new CpfRule(),
+                Rule::unique(Employee::class)->where('user_id', $this->user->id)->ignore($id),
+            ],
+            'city' => [
+                new RequiredIf(! $id),
+                'string',
+                'min:1',
+            ],
+            'state' => [
+                new RequiredIf(! $id),
+                'string',
+                'min:1',
+            ],
+        ];
+    }
+}
