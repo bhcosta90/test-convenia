2174ce9ec801d3b295750247ca4b4f284b27487f
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 17:15:54 -0300

    chore: enhance API documentation with security scheme configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 use Override;
@@ -22,6 +25,12 @@ final class AppServiceProvider extends ServiceProvider
         Scramble::configure()
             ->routes(function (Route $route) {
                 return str($route->uri)->startsWith('api/');
+            })->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->components->securitySchemes['bearer'] = SecurityScheme::http('bearer');
+
+                $openApi->security[] = new SecurityRequirement([
+                    'bearer' => [],
+                ]);
             });
     }
 }
