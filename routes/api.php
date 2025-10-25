5729774b55511f9522b9af9dfe7b170f7be89e10
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 09:22:55 -0300

    refactor API routes to use 'auth:api' middleware and update AuthController prefix

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function (): void {
+Route::middleware('auth:api')->group(function (): void {
     Route::apiResource('employees', Controllers\EmployeeController::class);
     Route::controller(Controllers\AuthController::class)
-        ->middleware(['auth:api'])
+        ->prefix('auth')
         ->group(function (): void {
             Route::post('login', 'login')->withoutMiddleware('auth:api');
             Route::post('refresh', 'refresh')->withoutMiddleware('auth:api');
