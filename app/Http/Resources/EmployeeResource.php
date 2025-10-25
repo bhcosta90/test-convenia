8835ee02b52c81a0389b12218851a288341cf8bc
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 08:22:28 -0300

    create EmployeeResource for transforming employee data into JSON format

---
+++ app/Http/Resources/EmployeeResource.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Employee */
+final class EmployeeResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'cpf' => $this->cpf,
+            'city' => $this->city,
+            'state' => $this->state,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
