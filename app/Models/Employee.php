a53bdc3ef6ff052cf48a68af602443d147c74955
Bruno Costa <bhcosta90@gmail.com>
2025-10-25 20:48:55 -0300

    feat: add observer for Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Observers\EmployeeObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ObservedBy([
+    EmployeeObserver::class,
+])]
 final class Employee extends Model
 {
     use HasFactory, SoftDeletes;
